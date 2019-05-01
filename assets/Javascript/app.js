0018ccbedf12056e19f6b3f9cf4096e565946ce4
Tomas <tslemenson@gmail.com>
2019-04-30 21:22:40 -0700

    added google login

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -143,38 +143,41 @@ function musicBrainzAPI() {
 }
 musicBrainzAPI()
 
-$(document).ready(function() {
-  function googleSignin() {
-    firebase.auth()
-    
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        console.log(token)
-        console.log(user)
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        console.log(errorCode)
-        console.log(errorMessage)
-      });
-
-  }
+function googleSignin() {
+  firebase.auth()
+  var provider = new firebase.auth.GoogleAuthProvider();
   
-  function googleSignout() {
-      firebase.auth().signOut()
-      
-      .then(function() {
-        console.log('Signout Succesfull')
-      }, function(error) {
-        console.log('Signout Failed')  
-      });
-  }
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(token)
+      console.log(user)
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      console.log(errorCode)
+      console.log(errorMessage)
+    });
+
+}
+
+function googleSignout() {
+    firebase.auth().signOut()
+    
+    .then(function() {
+      console.log('Signout Succesfull')
+    }, function(error) {
+      console.log('Signout Failed')  
+    });
+}
+
+$(document).ready(function() {
+  
 
 
 
