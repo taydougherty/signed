6d6de66fafc89c57c7432e905b3581d0901784c2
Alvin <alvinhewittalvin@gmail.com>
2019-05-02 18:24:47 -0700

    switched order on d3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -154,32 +154,6 @@ navigator.geolocation.getCurrentPosition(function(position) {
 
 })
 
-// D3 map
-"use strict";
-
-var format = d3.format(','); // Set tooltips
-
-var tip = d3.tip().attr('class', 'd3-tip').offset([-10, 0]).html(function (d) {
-    return "<strong>Country: </strong><span class='details'>".concat(d.properties.name, "<br></span><strong># of Unsiged Singers: </strong><span class='details'>").concat(format(d.population), "</span>");
-        // change d.population to {singer variable from ready function}
-});
-var margin = {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
-};
-var width = 960 - margin.left - margin.right;
-var height = 500 - margin.top - margin.bottom;
-var color = d3.scaleThreshold().domain([
-    1, 2, 4, 8, 12, 20, 28, 36, 44, 50
-    ]).range([
-    'rgb(247,251,255)', 'rgb(222,235,247)', 'rgb(198,219,239)', 'rgb(158,202,225)', 
-    'rgb(107,174,214)', 'rgb(66,146,198)', 'rgb(33,113,181)', 'rgb(8,81,156)', 'rgb(8,48,107)', 
-    'rgb(3,19,43)']);
-
-
-svg.call(tip);
 
 // ---------------------------------------- load web --------------------------------------------
 
@@ -229,13 +203,39 @@ $(document).ready(function (){
 
     // ------------------------------------ make the map -----------------------------------------------------
 
+    // D3 map
     // change all population related variable name into singer related name.
 
     // this need to be put into the html once user is logged in
+    // "use strict";
+
+    // var format = d3.format(','); // Set tooltips
+
+    // var tip = d3.tip().attr('class', 'd3-tip').offset([-10, 0]).html(function (d) {
+    //     return "<strong>Country: </strong><span class='details'>".concat(d.properties.name, "<br></span><strong># of Unsiged Singers: </strong><span class='details'>").concat(format(d.population), "</span>");
+    //         // change d.population to {singer variable from ready function}
+    // });
+    // var margin = {
+    //     top: 0,
+    //     right: 0,
+    //     bottom: 0,
+    //     left: 0
+    // };
+    // var width = 960 - margin.left - margin.right;
+    // var height = 500 - margin.top - margin.bottom;
+    // var color = d3.scaleThreshold().domain([
+    //     1, 2, 4, 8, 12, 20, 28, 36, 44, 50
+    //     ]).range([
+    //     'rgb(247,251,255)', 'rgb(222,235,247)', 'rgb(198,219,239)', 'rgb(158,202,225)', 
+    //     'rgb(107,174,214)', 'rgb(66,146,198)', 'rgb(33,113,181)', 'rgb(8,81,156)', 'rgb(8,48,107)', 
+    //     'rgb(3,19,43)']);
+
     // Note select('body') needs to select the parent element before the table
     // var svg = d3.select('#world-map').append('svg').attr('width', width).attr('height', height).append('g').attr('class', 'map');
     // var projection = d3.geoRobinson().scale(148).rotate([352, 0, 0]).translate([width / 2, height / 2]);
     // var path = d3.geoPath().projection(projection);
+
+    // svg.call(tip);
 
     // queue().
     // defer(d3.json, 'world_countries.json').
